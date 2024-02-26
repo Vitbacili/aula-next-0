c05c8d8c269a11896df4099cad22b257c68dac8e
Vitbacili <135022354+Vitbacili@users.noreply.github.com>
2024-02-26 09:36:38 -0300

    Comentário

--- app/page.tsx
+++ app/page.tsx
@@ -16,18 +16,18 @@ export default function Home() {
             rel="noopener noreferrer"
           >
             By{" "}
-            <Image
+            {/* <Image
               src="/vercel.svg"
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
               priority
-            />
+            /> */}
+            <h1>Aula Next 1</h1>
           </a>
         </div>
       </div>
-
       <div className={styles.center}>
         <Image
           className={styles.logo}
