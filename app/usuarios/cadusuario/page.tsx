e2a3dcb2e3eb81359e2c519d0b0ddfc6a64468cf
Vitbacili <135022354+Vitbacili@users.noreply.github.com>
2024-03-11 11:46:14 -0300

    aula 01 11/03

--- app/usuarios/cadusuario/page.tsx
+++ app/usuarios/cadusuario/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
+
 export default function Cadusuario () {
     return(
         <>
            <h1>Cadastro</h1>
            <p>Insira as informações do usuário</p>
+           
+           <p><Link href={'/'}>Voltar</Link></p>
         </>
     );
 }
