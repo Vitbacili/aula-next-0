819ce503dbc83dd5f7d5a06b4f7a2810765b7a44
Vitbacili <135022354+Vitbacili@users.noreply.github.com>
2024-03-18 11:39:47 -0300

    aula 18/03

--- app/usuarios/[id]/page.tsx
+++ app/usuarios/[id]/page.tsx
@@ -4,7 +4,7 @@ export default function PerfilUsuario ({ params }: { params: { id: string }}) {
     return(
         <>
            <h1>Perfil</h1>
-           <p>Nome do usuário</p>
+           <p>Nome do usuário {params.id}</p>
            
            <p><Link href={'/usuarios'}>Voltar</Link></p>
         </>
---
+++ src/usuarios.js
@@ -0,0 +1,20 @@
+const usuarios = [
+    {
+        id: 1,
+        nome: 'Josefo Jones',
+        altura: 1.96,
+        ativo: true
+    },
+    {
+        id: 2,
+        nome: 'Joelson Jair',
+        altura: 1.54,
+        ativo: false
+    },
+    {
+        id: 3,
+        nome: 'Jaquila Jaca',
+        altura: 1.77,
+        ativo: true
+    },
+];
